ad540ba4db1c1f969908951b1332a6760c3a2ba5
YeinerElismer <elismerolivera@gmail.com>
2016-05-17 11:59:15 -0500

    1

--- MyBatis3/src/Test.java
+++ MyBatis3/src/Test.java
@@ -20,16 +20,14 @@ public class Test {
             reader = Resources.getResourceAsReader(resourse);
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
             sqlSession = sqlSessionFactory.openSession();
-//            
-//            Ciudad ciudad= (Ciudad) (sqlSession.selectOne("Ciudad.SeleccionarCiudad",4));
-//            System.out.println("ciudad :"+ciudad.getNombre());
-//            
-//            List<Cliente> clientes  = sqlSession.selectList("Cliente.SeleccionarClientes","%%");
-//            for (int i = 0; i < clientes.size(); i++) {
-////                System.out.println("Cliente : "+clientes.get(i).getNombre());
-////                System.out.println("Ciudad : "+ clientes.get(i).getoCiudad().getNombre());
-//                System.out.println(clientes.get(i).getNombre()+" \t\t "+clientes.get(i).getoCiudad().getNombre());
-//            }
+////            
+////            Ciudad ciudad= (Ciudad) (sqlSession.selectOne("Ciudad.SeleccionarCiudad",4));
+////            System.out.println("ciudad :"+ciudad.getNombre());
+            
+            List<Cliente> clientes  = sqlSession.selectList("Cliente.SeleccionarClientes","%%");
+            for (int i = 0; i < clientes.size(); i++) {
+                System.out.println(clientes.get(i).getNombre()+" \t\t "+clientes.get(i).getoCiudad().getNombre()+" \t \t\t"+clientes.get(i).getoCiudad().getoPais().getNombre());
+            }
             
             //////
             
@@ -44,22 +42,22 @@ public class Test {
 //            
             
             //////
-             List<Pais> paises  = sqlSession.selectList("Pais.SeleccionarPaises","%%");
-            for (int i = 0; i < paises.size(); i++) {
-                System.out.println("PAIS : "+paises.get(i).getNombre());
-                System.out.println("***ciudades**");
-                
-                for (int j = 0; j < paises.get(i).getlCiudad().size(); j++) {
-                    System.out.println(paises.get(i).getlCiudad().get(j).getNombre());
-                    System.out.println("**Clientes***");
-                    
-                    for (int k = 0; k < paises.get(i).getlCiudad().get(j).getlCliente().size(); k++) {
-                    System.out.println(paises.get(i).getlCiudad().get(j).getlCliente().get(k).getNombre());                    
-                }
-                    
-                    
-                }
-            }
+//             List<Pais> paises  = sqlSession.selectList("Pais.SeleccionarPaises","%%");
+//            for (int i = 0; i < paises.size(); i++) {
+//                System.out.println("PAIS : "+paises.get(i).getNombre());
+//                System.out.println("***ciudades**");
+//                
+//                for (int j = 0; j < paises.get(i).getlCiudad().size(); j++) {
+//                    System.out.println(paises.get(i).getlCiudad().get(j).getNombre());
+//                    System.out.println("**Clientes***");
+//                    
+//                    for (int k = 0; k < paises.get(i).getlCiudad().get(j).getlCliente().size(); k++) {
+//                    System.out.println(paises.get(i).getlCiudad().get(j).getlCliente().get(k).getNombre());                    
+//                }
+//                    
+//                    
+//                }
+//            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
